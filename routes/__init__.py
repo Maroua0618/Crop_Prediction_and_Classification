e71fbd86bb29049ae4b549fe646ade3f35ad5c8b
Maroua <157848712+Maroua0618@users.noreply.github.com>
2025-08-28 19:01:10 +0200

    Fixed the circular import

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,11 +2,11 @@ from flask import Blueprint
 
 from .auth_routes import auth_bp
 from .main_routes import main_bp
-from .prediction_routes import prediction_bp
 from .classification_routes import classification_bp
+from .prediction_routes import prediction_bp
 
 def init_routes(app):
     app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
-    app.register_blueprint(prediction_bp)
     app.register_blueprint(classification_bp)
+    app.register_blueprint(prediction_bp)
